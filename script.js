91a36897f5b9842ad329b570655b945eae8671a3
sleepless-gambler <diazben46@gmail.com>
2026-03-28 10:48:04 -0400

    Create script.js with JavaScript code from index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript Code from index.html
+
+// Document ready function
+$(document).ready(function() {
+    console.log("Document is ready!");
+    // Additional JavaScript can go here
+});
+
